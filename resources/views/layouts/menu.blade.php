4284ef023383a2d9a2e1ac00a3abbbfb66029805
asdf50003206 <asdf500032061@gmail.com>
2020-11-26 21:22:03 +0800

    123

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,7 +1,12 @@
+
 <div class="col-1 menu">
-    <div><a href="/inventory/1"><span><h4>倉儲管理</h4></span></a></div>
+    <div class="row justify-content-center">
+    <div class="col-12 align-self-center">
+    <div class=""><a href="/inventory/1"><span><h4>倉儲管理</h4></span></a></div>
     <div class=""><a href="/inventory/1"><span>庫存清單</span></a></div>
     <div class=""><a href="/history"><span>歷史紀錄</span></a></div>
     <div class=""><a href="/purchase"><span>進貨動作</span></a></div>
     <div class=""><a href="/shipment"><span>出貨動作</span></a></div>
+    </div>
+</div>
 </div>
